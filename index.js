a1ac03be7e41989dba53521d2f84c4bab4fed89e
Stephen Tanksley <stephen.tanksley@gmail.com>
2020-02-11 14:45:58 -0600

    Completed the basic tutorial for how to make a chat application using websockets.

--- index.js
+++ index.js
@@ -3,21 +3,13 @@ require('dotenv').config();
 const express = require('express')
 const socket = require('socket.io')
 
-
+// App setup
 const app = express()
 // const PORT = process.env.PORT || 5000;
 const server = app.listen(5000, () => {
     // console.log(`\n ** Running on http://localhost:${PORT}... ** \n`)
     console.log(`\n ** Running on http://localhost:5000... ** \n`)
-
 })
-
-// app.use((error, req, res, next) => {
-//     console.log('error: ', error)
-//     res.status(500).json({
-//         message: "Something went horribly wrong."
-//     })
-// })
 
 //static files.
 app.use(express.static('public'));
@@ -25,11 +17,17 @@ app.use(express.static('public'));
 //socket setup
 const io = socket(server)
 io.on('connection', (socket) => {
+    
     console.log('Made socket connection', socket.id)
 
-    //when we get a 'chat' message
+    //handle chat event
     socket.on('chat', (data) => {
         //we want to transmit what we get from one socket to the rest of the open sockets connected to the server.
         io.sockets.emit('chat', data)
+    })
+
+    //handle typing event
+    socket.on('typing', (data) => {
+       socket.broadcast.emit('typing', data)
     })
 })
--- public/chat.js
+++ public/chat.js
@@ -10,16 +10,29 @@ let message = document.getElementById('message')
 let handle = document.getElementById('handle')
 let btn = document.getElementById('send')
 let output = document.getElementById('output')
+let feedback = document.getElementById('feedback')
 
 //emit events
-
-btn.addEventListener('submit', () => {
-    socket.emit('chat', {
-        message: message.value,
-        handle: handle.value 
+    btn.addEventListener('click', () => {
+        console.log(message.value, handle.value)
+        socket.emit('chat', {
+            message: message.value,
+            handle: handle.value 
+        })
+        message.value = "";
     })
-})
 
-socket.on('chat', (data) => {
-    output.innerHTML += '<p><strong>' + data.handle + ': </strong>' + data.message + '</p>'
-})
+    //when there's a person typing, we capture the 'typing' event along with the handle name of the user doing the typing.
+    message.addEventListener('keypress', () => {
+        socket.emit('typing', handle.value)
+    })
+
+//listening for events
+    socket.on('chat', (data) => {
+        feedback.innerHTML = '';
+        output.innerHTML += '<p><strong>' + data.handle + ': </strong>' + data.message + '</p>'
+    })
+
+    socket.on('typing', (data) => {
+        feedback.innerHTML = `<p><em> ${data} is typing a message... </em></p>`
+    })
